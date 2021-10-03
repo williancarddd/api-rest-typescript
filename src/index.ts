8d095c0133e3b5734882b214dcf2133503399455
William Cardoso <williancard123@gmail.com>
2021-10-03 10:01:24 -0400

    implemented cors security

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import { routers } from './routes/index.routes'
-
+import cors from 'cors'
 const app = express()
 
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(routers)
 
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("❤️ server listening port " + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 5500, () => {
+  console.log("❤️ server listening port " + (process.env.PORT || 5500))
 })
